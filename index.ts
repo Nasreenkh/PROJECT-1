5685777178fe7af9cb882d66e2d663615245e62d
Nasreen khan <160172100+Nasreenkh@users.noreply.github.com>
2024-05-17 02:04:21 +0500

    This is Cli-Number-Guessing Game.
    This is Cli-Number-Guessing Game.

---
+++ index.ts
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+// 1) computer generate a random number
+// 2) user input for guessing number
+// 3) compare user input or computer generated number and show result - done
+{
+    const randomNumber = Math.floor(Math.random( ) * 10 + 1);
+    //console.log(randomNumber); if you uncommint answer is given you without guessing no before question
+    const answer = await inquirer.prompt([
+        {
+            name: "userGuessedNumber",
+            type: "number",
+            message: "Please guess a number between 1-10:",
+        },
+    ]);
+
+    if (answer.userGuessedNumber === randomNumber) {
+        console.log("Congratulations! You guessed the right number.");
+    } else {
+        console.log("You guessed the wrong number.");
+    }
+}
+
